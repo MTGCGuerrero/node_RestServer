9a9ac8d33a6746784e4a513deca36e271eb985f7
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-05-29 13:46:20 -0400

    initialize server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hola mundo');
+require('dotenv').config();
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log('Servidor corriendo en', process.env.PORT);
+});
